6705691e030b9512e681276eb0d8b3fdd5dcf1ab
Eduardo <eduangelinicarvalho@gmail.com>
2023-08-24 10:56:25 -0300

    #2 - MUI and admin panel

---
+++ src/paginas/Administracao/Restaurantes/FormularioRestaurante.tsx
@@ -0,0 +1,38 @@
+import { Button, TextField } from '@mui/material'
+import axios from 'axios'
+import { useState } from 'react'
+
+export const FormularioRestaurante = () => {
+
+  const [nomeRestaurante, setNomeRestaurante] = useState('')
+
+  const aoSubmeterForm = (evento: React.FormEvent<HTMLFormElement>) => {
+    evento.preventDefault()
+
+    axios.post('http://localhost:8000/api/v2/restaurantes/', {
+      nome: nomeRestaurante
+    })
+      .then(resposta => {
+        alert('Restaurante cadastrado com sucesso!')
+      })
+      .catch(erro => console.log(erro))
+  }
+
+
+  return (
+    <form onSubmit={aoSubmeterForm}>
+      <TextField
+        value={nomeRestaurante}
+        onChange={evento => setNomeRestaurante(evento.target.value)}
+        label="Nome do Restaurante"
+        variant="standard"
+      />
+      <Button
+        type='submit'
+        variant="outlined"
+      >
+        Salvar
+      </Button>
+    </form>
+  )
+}
